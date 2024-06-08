a0c024887bf54a903381c9a6992edfc72cf84c91
Tensaey Solomon <12tensaeysolomon3@gmail.com>
2024-06-08 21:27:52 +0300

    firebase_fix

--- exp/lib/main.dart
+++ exp/lib/main.dart
@@ -1,13 +1,16 @@
 import "package:bloc/bloc.dart";
 import "package:exp/app.dart";
 import "package:firebase_core/firebase_core.dart";
+import "firebase_options.dart";
 import "package:flutter/material.dart";
 import "simple_bloc_observer.dart";
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   print('Initializing Firebase...');
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   print('Firebase initialized');
   Bloc.observer = SimpleBlocObserver();
   print('Bloc observer set');
